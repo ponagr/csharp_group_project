76f07c746f88aff40608f70135a432ed2e5e6bc1
Henrik <henrikpilback@gmail.com>
2024-10-23 13:59:39 +0200

    Lade till en player och skickar emd player som argument till metoderna print och move

--- Program.cs
+++ Program.cs
@@ -3,11 +3,12 @@
     static void Main(string[] args)
     {
         bool gameOver = false;
-
+        Player player = new Player("player");
         while (!gameOver)
         {
-            GameLevel.PrintGameBoard(GameLevel.gameLevel1);
-            GameLevel.MovePlayer(GameLevel.gameLevel1);
+            GameLevel.PrintGameBoard(GameLevel.gameLevel1, player);
+            GameLevel.MovePlayer(GameLevel.gameLevel1, player);
+            player.ShowHp();
         }
         
     }
