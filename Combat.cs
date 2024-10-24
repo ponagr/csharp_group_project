949b44e70be7c3c33a48f3b5cae172bddea3ed98
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2024-10-24 14:54:39 +0200

    Ändrat lite i utskriften i Combat, flyttat en enemy närmare på kartan så man snabbt kan komma in i combat

--- Combat.cs
+++ Combat.cs
@@ -17,36 +17,46 @@ public static class Combat
         while (true)
         {
             Console.Clear();
+
+            //HP
             Console.SetCursorPosition(0, 0);  //Rad, Kolumn
             HealthBar.PrintPlayerHealthBar(player);
             Console.SetCursorPosition(0, 1);
             player.ShowHp();    //Ska uppdateras
-            
+
             Console.SetCursorPosition(30, 0);
             HealthBar.PrintEnemyHealthBar(enemy);
             Console.SetCursorPosition(30, 1);
             enemy.ShowHp();     //Ska uppdateras
-            
+
             Console.WriteLine();
 
-            Console.SetCursorPosition(30, 2);
+            //Damage
+            Console.SetCursorPosition(16, 3);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(playerDamage);
-            Console.SetCursorPosition(30, 3);
+            Console.SetCursorPosition(16, 4);
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(enemyDamage);
             Console.ResetColor();
-            
 
+            //Gubben
             Console.SetCursorPosition(0, 2);
+            Player.PrintPlayerCharacter();
+            Console.SetCursorPosition(25, 2);
+            Enemy.PrintEnemyCharacter();
+
+            //Menyn
+            Console.SetCursorPosition(0, 8);
             Console.WriteLine("1. Attack");
+            Console.SetCursorPosition(0, 9);
             Console.WriteLine("2. Heal");
+            Console.SetCursorPosition(0, 10);
             Console.WriteLine("3. Fly");
-            Console.SetCursorPosition(0, 6);
+            Console.SetCursorPosition(0, 12);
             Console.Write("Input: ");
 
-            
-            
+
             string input = Console.ReadLine();
             switch (input)
             {
@@ -66,7 +76,16 @@ public static class Combat
 
             if (enemy.CurrentHp == 0)
             {
+                Console.SetCursorPosition(30, 0); // VISAR HEALTHBAREN FÖR ATT KUNNA SE NÄR HAN E DÖD
+                HealthBar.PrintEnemyHealthBar(enemy);
+                Console.SetCursorPosition(30, 1);
+                Console.Write("  ");
+                enemy.ShowHp();
+                Console.SetCursorPosition(0, 8);
                 player.EnemyKilled(enemy);  //Skriver ut skadan
+                Console.WriteLine("            ");
+                Console.WriteLine("            "); // FÖR ATT DÖLJA TIDIGARE TEXT
+                Console.WriteLine("            ");
                 Console.ReadKey();
                 return;
             }
